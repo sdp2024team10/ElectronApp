2b01aa9a29f07640817c37d37289cff1ea2b9e54
Jordan Andrade <jordanandrad@umass.edu>
2023-11-13 22:56:17 -0500

    Added LaTeX rendering using KaTeX

--- src/script.js
+++ src/script.js
@@ -1,1 +1,12 @@
-console.log("Welcome");
+function renderLatex(latexString) {
+    try {
+        document.getElementById('latex-output').innerHTML = katex.renderToString(latexString, {
+            throwOnError: false
+        });
+    } catch (error) {
+        console.error("Error rendering LaTeX:", error);
+    }
+}
+
+// Example usage
+renderLatex("y = x^2 + \\frac{x}{2} + 85");
